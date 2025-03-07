aad1e0e4a10856114a60f4bfd61d4ff524fe9375
MeybelineCorvera <ccabigai48l@gmail.com>
2025-03-06 22:18:03 -0600

    Add project files.

---
+++ PRACT2CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRACT2CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
